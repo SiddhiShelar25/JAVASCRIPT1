ca6a06730f9a33d639e19843d19396fa750b5303
Siddhi Shelar <siddhishelar.sknsits.comp@gmail.com>
2024-11-29 15:02:33 +0530

    Loops done

--- LOOPS/continue.js
+++ LOOPS/continue.js
@@ -1,6 +1,6 @@
 for (let index = 1; index <= 20; index++) {
-  if (index == 5) {
-    console.log(`Detected 5`);
+  if (index == 6) {
+    console.log(`Detected 6`);
     continue;
   }
   console.log(`Value of i is ${index}`);
--- LOOPS/forOf.js
+++ LOOPS/forOf.js
@@ -5,7 +5,7 @@
 const arr1 = [1, 2, 3, 4, 5];
 
 for (const num of arr1) {
-  // console.log(num);
+  console.log(num);
 }
 
 const greetings = "Siddhi!";
