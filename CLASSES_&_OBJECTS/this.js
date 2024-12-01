1cf97d7ea75142bc026253a4c61298a08f03f54b
Siddhi Shelar <siddhishelar.sknsits.comp@gmail.com>
2024-12-01 13:02:30 +0530

    Basics

---
+++ CLASSES_&_OBJECTS/this.js
@@ -0,0 +1,17 @@
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn
+
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`);
+
+    }
+
+    return this
+}
+
+const userOne = new User("hitesh", 12, true)
+const userTwo = new User("ChaiAurCode", 11, false)
+console.log(userOne.constructor);
+console.log(userTwo);
